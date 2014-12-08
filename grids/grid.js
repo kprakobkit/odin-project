e0f4041ffbf3baa4461ed3b4fef16a24568effdf
Peter Prakobkit <kprakobkit@gmail.com>
2014-12-07 20:34:13 -0800

    complete generate grid via pure javascript

---
+++ grids/grid.js
@@ -0,0 +1,38 @@
+function View() {}
+
+View.prototype.generateGrid = function() {
+  var numOfRows = 5;
+  var gridsPerRow = 5;
+  var container;
+  var gridsElement;
+  var rowElement;
+  var grid;
+  var i;
+  var j;
+
+  gridsElement = document.createElement('div');
+  gridsElement.setAttribute('class', 'grids');
+
+  for (i = 0; i < numOfRows; i++) {
+    rowElement = document.createElement('div');
+    rowElement.setAttribute('class', 'row');
+
+    for (j = 0; j < gridsPerRow; j++) {
+      gridElement = document.createElement('div');
+      gridElement.setAttribute('class', 'grid');
+      rowElement.appendChild(gridElement);
+    }
+
+    gridsElement.appendChild(rowElement);
+  }
+
+  container = document.getElementsByClassName('container')[0];
+  container.appendChild(gridsElement);
+};
+
+window.onload = function() {
+  view = new View();
+  view.generateGrid();
+};
+
+
